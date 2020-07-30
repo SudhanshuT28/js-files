32550831ee017da05d622f7989d1df0d93b539be
Sudhanshu <sid28.tripathi@gmail.com>
2020-07-31 02:36:26 +0530

    First Commit

---
+++ learn-array.js
@@ -0,0 +1,39 @@
+// var friends = ["Shimona","Saurabh","Sudhanshu"];
+// var naughtyList = [];
+// naughtyList.push(friends[2]);
+// console.log(naughtyList);
+// var index = naughtyList.indexOf("Sudhanshu");
+// if (index > -1) {
+//     naughtyList.splice(index,1);
+// }
+// console.log(naughtyList);
+
+// var households = ["Mango","Banana","Spinach","Carrot","chickpeas","Rice"];
+// var vegetables = [];
+// var fruits = [];
+// var cereals = [];
+// vegetables.push(households[2]);
+// vegetables.push(households[3]);
+// fruits.push(households[0]);
+// fruits.push(households[1]);
+// cereals.push(households[4]);
+// cereals.push(households[5]);
+// console.log(vegetables,fruits,cereals);
+
+// var arr1 = [];
+// var arr2 = [];
+
+// for (var x = 1; x <= 10; x++) {
+//     arr1.push(x);}
+
+// for (var x = 10; x > 0; x--) {
+//     arr2.push(x);}
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+
+
+
+
